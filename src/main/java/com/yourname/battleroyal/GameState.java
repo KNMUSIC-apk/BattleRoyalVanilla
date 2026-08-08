0314331f54bc8c32ad99856efe3ee2e75587f7e2
KNMUSIC-apk <music.vn.mc@gmail.com>
2026-08-09 06:21:12 +0700

    Fix formatting in GameState enum

--- src/main/java/com/yourname/battleroyal/GameState.java
+++ src/main/java/com/yourname/battleroyal/GameState.java
@@ -1,7 +1,7 @@
 package com.yourname.battleroyal;
 
 public enum GameState {
-    WAITING,      // Chờ người chơi join
-    COUNTDOWN,    // Đếm ngược 10 phút
-    STARTED       // Đang diễn ra trận đấu
+    WAITING,
+    COUNTDOWN,
+    STARTED
 }
